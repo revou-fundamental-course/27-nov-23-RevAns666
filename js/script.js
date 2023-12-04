838c8c9f9b76a3a5c8d410221b6d86a7da7d0d02
RevAns666 <revanbagas61@gmail.com>
2023-12-04 03:56:45 -0800

    secpush

---
+++ js/script.js
@@ -0,0 +1,12 @@
+<script>
+    const alas = document.getElementById('alas')
+    const tinggi = document.getElementById('tinggi')
+    const hitung = document.getElementById('hitung')
+    hitung.addEventListener('click', function() {
+        let a = alas.value
+        let t = tinggi.value
+        let l = 0.5*a*t
+        output.innerHTML = `Luas segitiga dari alas $(a) dan tinggi $(t) adalah $(l)`
+    
+    })
+</script>
