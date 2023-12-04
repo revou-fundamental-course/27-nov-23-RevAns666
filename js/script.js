ff9c0b6ac12bfe27598f7e7c052f4b2c34f706a8
RevAns666 <revanbagas61@gmail.com>
2023-12-04 07:42:46 -0800

    sesi js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-<script>
-    const alas = document.getElementById('alas')
-    const tinggi = document.getElementById('tinggi')
-    const hitung = document.getElementById('hitung')
-    hitung.addEventListener('click', function() {
-        let a = alas.value
-        let t = tinggi.value
-        let l = 0.5*a*t
-        output.innerHTML = `Luas segitiga dari alas $(a) dan tinggi $(t) adalah $(l)`
-    
-    })
-</script>
+/* hitung keliling segitiga */
+const sisi1 = document.getElementById('s1')
+const sisi2 = document.getElementById('s2')
+const sisi3 = document.getElementById('s3')
+const hitung1 = document.getElementById('hitung-keliling')
+const output1 = document.getElementById('output1')
+hitung1.addEventListener('click', function(){
+    let s1=sisi1.value
+    let s2=sisi2.value
+    let s3=sisi3.value
+    let h=s1*s2*s3
+    output1.innerHTML= `Hasil dari keliling= ${h}`
+})
+/* hitung luas segitiga */
+const alas = document.getElementById('alas')
+const tinggi = document.getElementById('tinggi')
+const hitung2 = document.getElementById('hitung-luas')
+const output2 = document.getElementById('output2')
+hitung2.addEventListener('click', function(){
+    let a=alas.value
+    let t=tinggi.value
+    let l=0.5*a*t
+    output2.innerHTML= `Alas= ${a}cm Tinggi= ${t}cm Luas= ${l}cm2`
+})
